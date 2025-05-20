b384e88cfd2490dadbc3c74e11760ec7d8c55baa
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-20 20:44:06 +0200

    footer

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,19 +1,21 @@
 export default function Header() {
-    return <header>
-        <div className="container d-flex align-items-center py-3">
-            <img src="dc-logo.png" alt="" />
-            <ul className="d-flex flex-row">
-                <li className="px-3">CHARACTERS</li>
-                <li className="px-3">COMICS</li>
-                <li className="px-3">MOVIES</li>
-                <li className="px-3">TV</li>
-                <li className="px-3">GAMES</li>
-                <li className="px-3">COLLECTIBLES</li>
-                <li className="px-3">VIDEOS</li>
-                <li className="px-3">FANS</li>
-                <li className="px-3">NEWS</li>
-                <li className="px-3">SHOP</li>
-            </ul>
-        </div>
+  return (
+    <header>
+      <div className="container d-flex flex-row justify-content-between align-items-center py-3">
+        <img src="dc-logo.png" alt="" />
+        <ul className="d-flex flex-row">
+          <li className="px-3">CHARACTERS</li>
+          <li className="px-3">COMICS</li>
+          <li className="px-3">MOVIES</li>
+          <li className="px-3">TV</li>
+          <li className="px-3">GAMES</li>
+          <li className="px-3">COLLECTIBLES</li>
+          <li className="px-3">VIDEOS</li>
+          <li className="px-3">FANS</li>
+          <li className="px-3">NEWS</li>
+          <li className="px-3">SHOP</li>
+        </ul>
+      </div>
     </header>
-};
+  );
+}
