379bcf4d8a29126abc2c9ff602162ac8433f14da
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-20 18:00:14 +0200

    footer social

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,26 @@
 export default function Footer() {
     return <footer>
-        footer
+        <section id="footer-link" className="d-flex flex-row">
+            <div>grgrgrg</div>
+            <img src="dc-logo-bg.png" alt="" />
+        </section>
+
+
+        <section id="footer-social">
+            <div className="container d-flex flex-row justify-content-between py-4">
+                <div>
+                    <button type="button" class="btn btn-outline-light border border-primary">SIGN-UP NOW!</button>
+                </div>
+                <div>
+                    <span className="text-primary px-3">FOLLOW US</span>
+                    <img src="footer-facebook.png" alt="" className="px-3" />
+                    <img src="footer-periscope.png" alt="" className="px-3" />
+                    <img src="footer-youtube.png" alt="" className="px-3" />
+                    <img src="footer-pinterest.png" alt="" className="px-3" />
+                    <img src="footer-twitter.png" alt="" className="px-3" />
+                </div>
+            </div>
+
+        </section>
     </footer>
 };
