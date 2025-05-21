11d7a3d9c44272e45f95867c84054af6000e91f5
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-21 14:47:16 +0200

    alert component test

--- src/component/layout/Main.jsx
+++ src/component/layout/Main.jsx
@@ -1,9 +1,11 @@
 import MainIcon from "./MainIcon";
+import Alert from "./user-interface/Alert";
 
 export default function Main() {
   return (
     <main>
       <div id="content">
+        <Alert />
         <div className="container py-5">content here</div>
       </div>
       <MainIcon />
