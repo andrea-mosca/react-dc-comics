c8508649823d61343f01dcb99109a75193a72f21
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-21 14:31:47 +0200

    bonus: icon main component

--- src/component/layout/Main.jsx
+++ src/component/layout/Main.jsx
@@ -1,55 +1,12 @@
+import MainIcon from "./MainIcon";
+
 export default function Main() {
   return (
     <main>
       <div id="content">
         <div className="container py-5">content here</div>
       </div>
-      <div id="main-link">
-        <div className="container py-5">
-          <div className="row align-items-center justify-content-between g-0">
-            <div className="col-2">
-              <img
-                className="img-main pe-2"
-                src="buy-comics-digital-comics.png"
-                alt=""
-              />
-              <span>DIGITAL COMICS</span>
-            </div>
-            <div className="col-2">
-              <img
-                className="img-main pe-2"
-                src="buy-comics-merchandise.png"
-                alt=""
-              />
-              <span>DC MERCHANDISE</span>
-            </div>
-            <div className="col-2">
-              <img
-                className="img-main pe-2"
-                src="buy-comics-subscriptions.png"
-                alt=""
-              />
-              <span>SUBSCRIPTION</span>
-            </div>
-            <div className="col-2">
-              <img
-                className="img-main pe-2"
-                src="buy-comics-shop-locator.png"
-                alt=""
-              />
-              <span>COMIC SHOP LOCATOR</span>
-            </div>
-            <div className="col-2">
-              <img
-                className="img-main pe-2"
-                src="buy-dc-power-visa.svg"
-                alt=""
-              />
-              <span>DC POWER VISA</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MainIcon />
     </main>
   );
 }
