ce333dbcbfa681818aff72402e572b04ecda0d90
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-22 17:13:00 +0200

    comics button control

--- src/component/layout/Main.jsx
+++ src/component/layout/Main.jsx
@@ -6,7 +6,17 @@ export default function Main() {
     <main>
       <div id="jumbotron-section"></div>
       <div id="content">
+        <div className="container">
+          <button type="button" class="btn btn-primary current-series-btn">
+            CURRENT SERIES
+          </button>
+        </div>
         <ProductList />
+        <div className="container d-flex justify-content-center pb-4">
+          <button type="button" class="btn btn-primary">
+            Load More
+          </button>
+        </div>
       </div>
       <MainIcon />
     </main>
