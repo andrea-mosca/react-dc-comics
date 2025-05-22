b21fa9a5ddc72bd4541bd56867b52411a2394d90
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-22 17:30:04 +0200

    header link hover

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -4,16 +4,36 @@ export default function Header() {
       <div className="container d-flex flex-row justify-content-between align-items-center py-3">
         <img src="dc-logo.png" alt="" />
         <ul className="d-flex flex-row">
-          <li className="px-3">CHARACTERS</li>
-          <li className="px-3">COMICS</li>
-          <li className="px-3">MOVIES</li>
-          <li className="px-3">TV</li>
-          <li className="px-3">GAMES</li>
-          <li className="px-3">COLLECTIBLES</li>
-          <li className="px-3">VIDEOS</li>
-          <li className="px-3">FANS</li>
-          <li className="px-3">NEWS</li>
-          <li className="px-3">SHOP</li>
+          <li className="px-3">
+            <a href="#">COMICS</a>
+          </li>
+          <li className="px-3">
+            <a href="#">MOVIES</a>
+          </li>
+          <li className="px-3">
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li className="px-3">
+            <a href="#">TV</a>
+          </li>
+          <li className="px-3">
+            <a href="#">GAMES</a>
+          </li>
+          <li className="px-3">
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li className="px-3">
+            <a href="#">VIDEOS</a>
+          </li>
+          <li className="px-3">
+            <a href="#">FANS</a>
+          </li>
+          <li className="px-3">
+            <a href="#">NEWS</a>
+          </li>
+          <li className="px-3">
+            <a href="#">SHOP</a>
+          </li>
         </ul>
       </div>
     </header>
