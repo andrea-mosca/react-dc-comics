e5d1621025e006c8e5d81282ba6a22c98b7711d4
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-22 16:49:50 +0200

    refactoring comic img

--- src/component/products/productsCard.jsx
+++ src/component/products/productsCard.jsx
@@ -1,10 +1,10 @@
 export default function ProductCard({ comic }) {
   return (
     <div className="col-2">
-      <div className="card">
-        <img src={comic.thumb} alt={comic.title}></img>
-        <div>{comic.title}</div>
+      <div className="comic-container">
+        <img className="comic-img" src={comic.thumb} alt={comic.title}></img>
       </div>
+      <div className="py-3">{comic.series}</div>
     </div>
   );
 }
